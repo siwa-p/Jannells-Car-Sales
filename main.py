5f617bde8982f136a4ef399bde06a6efafd4dbcf
siwa-p <prasiwakoti@gmail.com>
2025-06-12 20:53:40 -0400

    rebuilt using session and good data practice with sqlalchemy

--- main.py
+++ main.py
@@ -1,32 +1,112 @@
 import os
+import csv
 import pandas as pd
-from sqlalchemy import create_engine
-from load_db import load_all_pandas, load_all_sqlalchemy, load_csv_pandas, load_csv_sqlalchemy
+from sqlalchemy import create_engine, Column, String, Boolean, Integer
+from sqlalchemy.ext.declarative import declarative_base
+from load_db import get_data
 from test import test_db
+from sqlalchemy.orm import sessionmaker
+
+user = os.getenv('PG_USER')
+password = os.getenv('PG_PASSWORD')
+host = os.getenv('PG_HOST')
+port = os.getenv('PG_PORT')
+db = os.getenv('PG_DB')
+csv_name = 'data/client_contact_status.csv'
+engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}',echo=True)
+
+Base = declarative_base()
+
+# define the database model
+class Clients(Base):
+    __tablename__ = 'clients'
+    id = Column('id', String, primary_key=True)
+    company = Column('company', String)
+    name = Column('name', String)
+    address = Column('address', String)
+    email = Column('email', String)
+    phone = Column('phone', String)
+    sales_rep = Column('sales_rep', String)
+
+    def __repr__(self):
+        return f"<Clients(id='{self.id}', company='{self.company}', name='{self.name}', address='{self.address}', email='{self.email}', phone='{self.phone}', sales_rep='{self.sales_rep}')>"
+
+class clientcontactstatus(Base):
+    __tablename__ = 'client_contact_status'
+    id = Column('id', String, primary_key=True)
+    name = Column('name', String)
+    can_email = Column('can_email', Integer)
+    can_call = Column('can_call', Integer)
+
+    def __repr__(self):
+        return f"<ClientContactStatus(id='{self.id}', name='{self.name}', can_email={self.can_email}, can_call={self.can_call})>"    
+
+
+def insert_clients(data):
+    try:
+        client_items = [Clients(**datum) for datum in data]
+        session.add_all(client_items)
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(f"Error inserting data: {e}")
+
+def insert_csv(data):
+    for d in data:
+        for key, value in d.items():
+            if value == 'null':
+                d[key] = None
+    try:
+        csv_items = [clientcontactstatus(**datum) for datum in data]
+        session.add_all(csv_items)
+        session.commit()
+    except Exception as e:
+        session.rollback()
+        print(f"Error inserting data: {e}")
+
+def load_api(table_name:str):
+    is_data = None
+    sample_data = get_data(0,5,table_name)
+    if sample_data:
+        is_data = True
+    offset = 0
+    limit = 10
+    while is_data:
+        data_queried = get_data(offset, limit, table_name)
+        if not data_queried:
+            is_data = False
+        else:
+            insert_clients(data_queried)
+            offset += limit
+
+def load_csv(filepath:str):
+    data_read = []
+    with open(filepath, 'r') as file:
+        reader = csv.DictReader(file)
+        for row in reader:
+            # print(row)
+            data_read.append(row)
+    insert_csv(data_read)
+    return None
+
 
 if __name__ == '__main__':
-    user = os.getenv('PG_USER')
-    password = os.getenv('PG_PASSWORD')
-    host = os.getenv('PG_HOST')
-    port = os.getenv('PG_PORT')
-    db = os.getenv('PG_DB')
-    csv_name = 'data/client_contact_status.csv'
-    engine = create_engine(f'postgresql://{user}:{password}@{host}:{port}/{db}')
     
-    # load_all_pandas('people', engine)
-    # load_all_pandas('clients', engine)
-    # load_csv_pandas(csv_name, engine)
-    
-    # load_all_sqlalchemy('people', engine)
-    load_all_sqlalchemy('clients', engine)
-    load_csv_sqlalchemy(csv_name, engine)    
-    
+    # Create tables if they do not exist
+
+    Session = sessionmaker(bind = engine)
+    session = Session()
+    Base.metadata.create_all(engine)
+    load_api('clients')
+    load_csv(csv_name)
+    session.close()
+
     # # run sql from stored procs
     # # run table_overview.sql 
     
-    # # now use this SELECT * FROM client_contact_status_view to convert to csv
+    # now use this SELECT * FROM client_contact_status_view to convert to csv
     # df = pd.read_sql("SELECT * FROM client_contact_status_view", engine)
     # df.to_csv("data/client_contact_status_view.csv", index=False)
     
-    # # finally test
+    # finally test
     # test_db()
--- utils.py
+++ utils.py
@@ -1,6 +1,3 @@
-from sqlalchemy import create_engine, insert, Table, MetaData, Column, String, Boolean, Integer
-import pandas as pd
-
 
 # save to .env and replace existing
 def write_to_dotenv(json_data):
@@ -23,59 +20,3 @@ def check_status_code(response):
     else:
         print(f"Unauthorized access. Status code: {status_code}")
         return False
-
-
-def json_to_postgres_pandas(data, table_name: str, engine):
-    data_df = pd.DataFrame(data)
-    data_df.to_sql(name=table_name, con=engine, if_exists='append', index=False)
-
-
-def json_to_postgres_sqlalchemy(data, table_name:str, engine):
-    # data is a list of dictionaries
-    metadata = MetaData()
-    if metadata.tables:
-        table = metadata.tables[table_name]
-    else:
-        table = Table(
-            table_name,
-            metadata,
-            Column('id', String, primary_key=True),
-            Column('company', String),
-            Column('name', String),
-            Column('address', String),
-            Column('email', String),
-            Column('phone', String),
-            Column('sales_rep', String),
-        )
-        metadata.create_all(engine) 
-    with engine.connect() as connection:
-        for d in data:
-            stmt = insert(table).values(d)
-            connection.execute(stmt)
-        connection.commit()
-    
-# the columns are hardcoded, hence the need for two methods
-def json_to_postgres_csv(data, table_name:str, engine):
-    # data is a list of dictionaries
-    metadata = MetaData()
-    if metadata.tables:
-        table = metadata.tables[table_name]
-    else:
-        table = Table(
-            table_name,
-            metadata,
-            Column('id', String, primary_key=True),
-            Column('name', String),
-            Column('can_email', Integer),
-            Column('can_call', Integer),
-        )
-        metadata.create_all(engine) 
-    with engine.connect() as connection:
-        for d in data:
-            for key, value in d.items():
-                if value == 'null':
-                    d[key] = None
-            stmt = insert(table).values(d)
-            connection.execute(stmt)
-        connection.commit()
-
